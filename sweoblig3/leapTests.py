504896f1593c0c411a06002a634b0be6f375a877
Kubilay Kerim <kubioz@icloud.com>
2021-11-03 19:37:24 +0100

    Commit files from oblig 2

---
+++ sweoblig3/leapTests.py
@@ -0,0 +1,27 @@
+# Should pass as 2000 is in fact a leap year
+import pytest
+
+from leapYear import leapYear
+
+def test_DivisibleBy4Not100():
+    assert leapYear(1800) % 4 == 0 and leapYear(1800) % 100 == False
+
+def test_DivisibleBy400():
+    assert leapYear(1600) % 400 == True
+
+
+def test_NotDivisibleBy4():
+    assert leapYear(1598) % 4 == False
+
+
+def test_DivisibleBy100Not400():
+    assert leapYear(1000) % 100 == 0 and leapYear(1000) % 400 == False
+
+
+
+
+
+
+
+
+
---
+++ sweoblig3/leapYear.py
@@ -0,0 +1,7 @@
+
+def leapYear(year):
+    if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+        return True
+    else:
+        return False
+
